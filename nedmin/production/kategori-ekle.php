cb6b40beff90956a76570f6ee2e1f34efaa2180f
Murad268 <82538242+Murad268@users.noreply.github.com>
2022-10-22 12:14:38 +0400

    ss

--- nedmin/production/kategori-ekle.php
+++ nedmin/production/kategori-ekle.php
@@ -14,7 +14,7 @@ include 'header.php';
       <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="x_panel">
           <div class="x_title">
-            <h2>Kategori Düzenleme <small>,
+            <h2>Kategoriya Əlavə Etmək <small>,
 
               <?php 
 
--- sidebar.php
+++ sidebar.php
@@ -1,24 +1,26 @@
 <div class="col-md-3"><!--sidebar-->
 				<div class="title-bg">
-					<div class="title">Categories</div>
+					<div class="title">Kateqoriyalar</div>
 				</div>
 				
 				<div class="categorybox">
 					<ul>
-						<li><a href="category.htm">Women Accessories</a></li>
-						<li><a href="category.htm">Men Shoes</a></li>
-						<li><a href="category.htm">Gift Specials</a></li>
-						<li><a href="category.htm">Electronics</a>
-							<ul>
-								<li><a href="#">iPhone 4S</a></li>
-								<li><a href="#">Samsung Galaxy</a></li>
-								<li><a href="#">MacBook Pro 17"</a></li>
-							</ul>
-						</li>
-						<li><a href="category.htm">On sale</a></li>
-						<li><a href="category.htm">Summer Specials</a></li>
-						<li><a href="category.htm">Electronics</a></li>
-						<li class="lastone"><a href="category.htm">Unique Stuff</a></li>
+						<?php
+							$kategoriyalariAlSorgusu = $db->prepare("SELECT * FROM categories");
+							$kategoriyalariAlSorgusu->execute();
+							$kagetoriler = $kategoriyalariAlSorgusu->fetchAll(PDO::FETCH_ASSOC);
+							$kategoriSayi = $kategoriyalariAlSorgusu->rowCount();
+							if($kategoriSayi>0) {
+								foreach($kagetoriler as $kategori) {?>
+										<li><a href="category.htm"><?=$kategori["category_ad"]?></a></li>
+								<?php
+								} 
+							} else {
+								echo "Heç bir kateqoriya yoxdur.";
+							}
+						?>
+				
+					
 					</ul>
 				</div>
 				
